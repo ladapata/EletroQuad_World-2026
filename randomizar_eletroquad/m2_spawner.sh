32f71386e1b9d2eaab25b7d4b332357f2a8b1016
ladapata <109905748+ladapata@users.noreply.github.com>
2026-03-07 19:36:30 -0300

    Add files via upload

--- randomizar_eletroquad/m2_spawner.sh
+++ randomizar_eletroquad/m2_spawner.sh
@@ -1,1 +1,32 @@
+#!/bin/bash
+
+# start-variables
+ball_and_hoses_height=1.6
+
+# end-variables
+# ======================================================================
+# start-functions
+generate_nums_within_range() {
+
+  echo $return_val
+}
+
+# end-functions
+# ======================================================================
+# start-script
+
+# end-script
+# ======================================================================
+# start-extras
 # comprimento do cabo = np.sin((points[i]-points[i-1])/2)*2*4.5
+
+# <!--line 28-->
+
+# <!--line 36-->
+# <!--line 41-->
+# <!--line 46-->
+
+# <!--line 52-->
+# <!--line 57-->
+# <!--line 62-->
+# end-extras
---
+++ randomizar_eletroquad/m3_spawner.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# start-variables
+manometer_height=0.85
+pointer_height_offset=0.8511
+pointer_alignment_offset=0.009
+general_rotation_degrees=-90
+
+# end-variables
+# ======================================================================
+# start-functions
+generate_nums_within_range() {
+
+  echo $return_val
+}
+
+# end-functions
+# ======================================================================
+# start-script
+
+# end-script
+# ======================================================================
+# start-extras
+# <!--line 25-->
+# <!--line 31-->
+
+# <!--line 38-->
+# <!--line 44-->
+
+# <!--line 51-->
+# <!--line 57-->
+# end-extras
